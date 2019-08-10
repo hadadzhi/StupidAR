f84ee6f04704289a229c245f1addcda2c9c2cbb0
hadadzhi <hadadzhi@gmail.com>
2019-08-10 18:09:26 +0300

    Queue flushing

--- StupidAR/BlockingQueue.h
+++ StupidAR/BlockingQueue.h
@@ -4,49 +4,96 @@
 #include <mutex>
 #include <condition_variable>
 
+/// Fixed-size, blocking queue with support for flushing
 template <typename T>
 class BlockingQueue {
 
 private:
 
-	size_t m_maxCount;
-	std::queue<T> m_items;
-	std::mutex m_lock;
-	std::condition_variable m_notFull;
-	std::condition_variable m_notEmpty;
+    bool m_flushing;
+    size_t m_size;
+    std::deque<T> m_items;
+    std::mutex m_lock;
+    std::condition_variable m_notFull;
+    std::condition_variable m_notEmpty;
 
 public:
 
-	explicit BlockingQueue(size_t maxCount)
-		: m_maxCount(maxCount) {
-	}
+    explicit BlockingQueue(size_t size)
+        : m_size(size), m_flushing(false) {
+    }
 
-	template <typename E, typename enable_if = typename std::enable_if_t<std::is_constructible_v<T, E>>>
-	void put(E&& e) {
-		std::unique_lock<std::mutex> l(m_lock);
-		m_notFull.wait(l, [&]() { return m_items.size() != m_maxCount; });
+    /// Blocks until not full or closed
+    /// Returns true if `e` was succesfully added to the queue
+    template <typename E, typename enable_if = typename std::enable_if_t<std::is_constructible_v<T, E>>>
+    bool put(E&& e) {
+        std::unique_lock<std::mutex> l(m_lock);
+        m_notFull.wait(l, [&]() { return m_items.size() != m_size || m_flushing; });
 
-		m_items.push(std::forward<E>(e));
-		m_notEmpty.notify_one();
-	}
+        if (m_flushing) {
+            return false;
+        }
 
-	T take() {
-		std::unique_lock<std::mutex> l(m_lock);
-		m_notEmpty.wait(l, [&]() { return m_items.size() != 0; });
+        m_items.push_back(std::forward<E>(e));
+        m_notEmpty.notify_one();
 
-		T r = std::move(m_items.front());
-		m_items.pop();
-		m_notFull.notify_one();
-		return r;
-	}
+        return true;
+    }
 
-	void poll(T& out) {
-		std::unique_lock<std::mutex> l(m_lock);
-		if (!m_items.empty()) {
-			out = std::move(m_items.front());
-			m_items.pop();
-			m_notFull.notify_one();
-		}
-	}
+    /// Blocks until not empty or closed
+    /// Returns true if `out` was succesfully initialized from the front of the queue
+    bool take(T& out) {
+        std::unique_lock<std::mutex> l(m_lock);
+        m_notEmpty.wait(l, [&]() { return m_items.size() != 0 || m_flushing; });
+
+        if (m_flushing) {
+            return false;
+        }
+
+        out = std::move(m_items.front());
+        m_items.pop_front();
+        m_notFull.notify_one();
+
+        return true;
+    }
+
+    /// Non-blocking take
+    /// Returns true if `out` was succesfully initialized from the front of the queue
+    bool poll(T& out) {
+        std::unique_lock<std::mutex> l(m_lock);
+
+        if (!m_items.empty()) {
+            out = std::move(m_items.front());
+            m_items.pop_front();
+            m_notFull.notify_one();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /// Closes the queue (`poll()`, `take()` and `put()` will fail until `end_flush()` is called)
+    /// Removes all enqueued elements
+    /// Releases all threads currently blocked in `take()` or `put()`
+    void begin_flush() {
+        std::unique_lock<std::mutex> l(m_lock);
+
+        if (m_flushing) {
+            return;
+        }
+
+        m_flushing = true;
+        m_items.clear();
+
+        m_notFull.notify_all();
+        m_notEmpty.notify_all();
+    }
+
+    /// Opens the queue (`put()`, `poll()` and `take()` will resume acting as normal)
+    void end_flush() {
+        std::unique_lock<std::mutex> l(m_lock);
+        m_flushing = false;
+    }
 
 };
